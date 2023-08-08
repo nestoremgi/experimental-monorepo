22f34926d9fe56f90969d4fd99574ae2d8e37924
nestorbfgo <nestor.martinez@bfgo.mx>
2023-08-07 18:26:31 -0600

    exp

--- packages/my-app/astro.config.mjs
+++ packages/my-app/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
-  outDir: '../../dist/packages/my-app',
+  
   output:'static',
   adapter: vercel(),
 });
