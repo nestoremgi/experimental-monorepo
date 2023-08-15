40fc35efb2a26bfac6f1387eeb3b89b16da35d68
nestorbfgo <nestor.martinez@bfgo.mx>
2023-08-14 18:19:26 -0600

    add

--- packages/my-app/astro.config.mjs
+++ packages/my-app/astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
-  
-  output:'server',
-  adapter: vercel(),
+  output: "server",
+  adapter: cloudflare()
 });
